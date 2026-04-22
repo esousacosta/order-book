1c6fb9c7dfd0a1d09937ffa9f17aeb5202233b19
Emanoel <victoremanoel23@gmail.com>
2026-04-22 08:23:55 +0200

    perf: add more insertion benchmarks

--- benchmarks/micro/insertion_bench.cpp
+++ benchmarks/micro/insertion_bench.cpp
@@ -7,19 +7,69 @@ static void BM_insertion_random_1KOrders(benchmark::State &state) {
     core::OrderBook book;
     benchmark::utils::populateRandomBook(book, 1000, 100, 10000, 1, 1000);
 
+    constexpr core::OrderId orderId{1001};
+    constexpr core::Price price{5000};
+    constexpr core::Quantity qty{100};
+    constexpr auto side{core::Side::Buy};
 
     for (auto _ : state) {
-        constexpr core::OrderId orderId{1001};
-        constexpr core::Price price{5000};
-        constexpr core::Quantity qty{100};
-        constexpr auto side{core::Side::Buy};
-
         book.addOrder(core::Order(orderId, price, qty, side));
-        book.cancelOrder(orderId);
+    }
+}
+
+static void BM_insertion_random_10KOrders(benchmark::State &state) {
+    core::OrderBook book;
+    benchmark::utils::populateRandomBook(book, 10000, 100, 10000, 1, 1000);
+    constexpr core::OrderId orderId{10001};
+    constexpr core::Price price{5000};
+    constexpr core::Quantity qty{100};
+    constexpr auto side{core::Side::Buy};
+    for (auto _ : state) {
+        book.addOrder(core::Order(orderId, price, qty, side));
+    }
+}
+
+static void BM_insertion_random_100KOrders(benchmark::State &state) {
+    core::OrderBook book;
+    benchmark::utils::populateRandomBook(book, 100000, 100, 10000, 1, 1000);
+    constexpr core::OrderId orderId{100001};
+    constexpr core::Price price{5000};
+    constexpr core::Quantity qty{100};
+    constexpr auto side{core::Side::Buy};
+    for (auto _ : state) {
+        book.addOrder(core::Order(orderId, price, qty, side));
+    }
+}
+
+static void BM_insertion_random_1MOrders(benchmark::State &state) {
+    core::OrderBook book;
+    benchmark::utils::populateRandomBook(book, 1000000, 100, 10000, 1, 1000);
+    constexpr core::OrderId orderId{1000001};
+    constexpr core::Price price{5000};
+    constexpr core::Quantity qty{100};
+    constexpr auto side{core::Side::Buy};
+    for (auto _ : state) {
+        book.addOrder(core::Order(orderId, price, qty, side));
+    }
+}
+
+static void BM_insertion_random_10MOrders(benchmark::State &state) {
+    core::OrderBook book;
+    benchmark::utils::populateRandomBook(book, 10000000, 100, 10000, 1, 1000);
+    constexpr core::OrderId orderId{10000001};
+    constexpr core::Price price{5000};
+    constexpr core::Quantity qty{100};
+    constexpr auto side{core::Side::Buy};
+    for (auto _ : state) {
+        book.addOrder(core::Order(orderId, price, qty, side));
     }
 }
 
 BENCHMARK(BM_insertion_random_1KOrders)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_insertion_random_10KOrders)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_insertion_random_100KOrders)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_insertion_random_1MOrders)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_insertion_random_10MOrders)->Unit(benchmark::kMicrosecond);
 
 // === Main Function ===
 BENCHMARK_MAIN();
